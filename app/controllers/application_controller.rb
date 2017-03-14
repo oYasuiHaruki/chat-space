68dc3fbdbd8566df7ec911c41df238cbc766ead6
yasui <yasui.haruki.74s@gmail.com>
2017-03-14 10:45:32 +0900

    application_contoroller.rbのファイルのインデントを合わせた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
-    def configure_permitted_parameters
+  def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  end
 
-    def after_sign_out_path_for(resorce)
+  def after_sign_out_path_for(resorce)
       '/users/sign_in'
-    end
+  end
 end
