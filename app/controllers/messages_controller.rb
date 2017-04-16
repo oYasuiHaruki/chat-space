00e7e873ba5e152fd3bc3b9f2cd04e316270a2cf
安井春輝 <yasui.haruki.74s@gmail.com>
2017-04-17 00:10:42 +0900

    ・renderを使用

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,31 @@
 class MessagesController < ApplicationController
 
+  before_action :index_variable, only:[:index, :create]
+
   def index
+  end
+
+  def create
+    @post_message = Message.new(message_params)
+    if @post_message.save
+      flash.now[:notice] = "メッセージが送信されました"
+      render action: :index
+    else
+      flash.now[:alert] = "メッセージを入力してください"
+      render action: :index
+    end
+
+  end
+
+  private
+
+  def index_variable
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @users = @group.users
     @message = Message.new
     @messages = @group.messages
   end
-
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-      flash[:notice] = "メッセージが送信されました"
-      redirect_to(group_messages_path)
-    else
-      flash[:alert] = "メッセージを入力してください"
-      redirect_to(group_messages_path)
-    end
-
-  end
-
-  private
 
   def message_params
     params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
