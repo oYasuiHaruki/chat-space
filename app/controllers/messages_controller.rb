a8b4ec23bf494e91a596a295b3f3f1b4c0206106
安井春輝 <yasui.haruki.74s@gmail.com>
2017-04-16 22:06:38 +0900

    ・メッセージ機能の搭載

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,24 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @users = @group.users
+    @message = Message.new
   end
 
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "メッセージが送信されました"
+      redirect_to(group_messages_path)
+    else
+      flash[:alert] = "メッセージを入力してください"
+      redirect_to(group_messages_path)
+    end
+
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
 
 end
