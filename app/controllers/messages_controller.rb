dda9794f23d736d8c0d67858153a042526b7d0ee
安井春輝 <yasui.haruki.74s@gmail.com>
2017-04-17 15:54:30 +0900

    ・メソッドの命名を変更 ・redirect_toに変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
 
-  before_action :index_variable, only:[:index, :create]
+  before_action :set_variables, only:[:index, :create]
 
   def index
   end
@@ -8,18 +8,17 @@ class MessagesController < ApplicationController
   def create
     @post_message = Message.new(message_params)
     if @post_message.save
-      flash.now[:notice] = "メッセージが送信されました"
-      render action: :index
+      flash[:notice] = "メッセージが送信されました"
+      redirect_to(group_messages_path)
     else
       flash.now[:alert] = "メッセージを入力してください"
       render action: :index
     end
-
   end
 
   private
 
-  def index_variable
+  def set_variables
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @users = @group.users
